18e9b7730c6caa2809f927ee119b7e8b642e5e5f
Pablo Velez <pvelez@bp-3.com>
2019-06-29 00:41:20 -0500

    Add the edit link, and add the delete method and link

--- web/router.ex
+++ web/router.ex
@@ -16,11 +16,13 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
-    get "/topics/:id/edit", TopicController, :edit
-    put "/topics/:id", TopicController, :update
+    # get "/", TopicController, :index
+    # get "/topics/new", TopicController, :new
+    # post "/topics", TopicController, :create
+    # get "/topics/:id/edit", TopicController, :edit
+    # put "/topics/:id", TopicController, :update
+    # delete "/topics/:id", TopicController, :delete
+    resources "/", TopicController # this replace all the above lines just following the rest conventions
   end
 
   # Other scopes may use custom stacks.
