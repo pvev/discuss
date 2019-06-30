07792faec0d6d781af0e7ec8846aff58bf394c91
Pablo Velez <pvelez@bp-3.com>
2019-06-30 12:55:13 -0500

    Add the auth controller and user model

--- web/models/user.ex
+++ web/models/user.ex
@@ -2,9 +2,9 @@ defmodule Discuss.User do
   use Discuss.Web, :model
 
   schema "users" do
-    field: :email, :string
-    field: :provider, :string
-    field: :token, :string
+    field :email, :string
+    field :provider, :string
+    field :token, :string
 
     timestamps()
   end
